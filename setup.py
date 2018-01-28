36bc315bc353190bd55a91b9dc46db4d56cf4bb3
Anatoly.Beskakotov <beskakotov.as@gmail.com>
2018-01-28 07:14:55 +0300

    Создана структура модуля python

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='mavr_module',
+      version='0.0.1',
+      description='SubModules for MAVR SAO RAN',
+      author='Anatoly Beskakotov',
+      author_email='beskakotov.as@gmail.com',
+      url='https://github.com/Black13Wolf/mavr_module',
+      packages=['mavr',],
+     )
