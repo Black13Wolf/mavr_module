9813657920fdb767c878c71aa3ae362d14b09e6a
Beskakotov_work <beskakotov.as@gmail.com>
2018-02-05 17:38:51 +0300

    mavr.processing: Изменена функция get_ps. setup.py: добавлены requirements

--- mavr/processing.py
+++ mavr/processing.py
@@ -1,5 +1,6 @@
-def get_ps(path_to_dat, diff=0, showing=False):
-    from numpy import memmap
+def get_ps(path_to_dat, diff=0, acf=False, save=False, showing=False):
+    from numpy import memmap, zeros, fft
+    from os.path import basename
     serie = memmap(path_to_dat, dtype='uint16')
     frames = serie.size()/512/512
     serie = serie.reshape((frames, 512, 512))
@@ -14,6 +15,13 @@ def get_ps(path_to_dat, diff=0, showing=False):
         from matplotlib.pyplot import imshow, show
         imshow(ps, cmap='gray')
         show()
-    
-def get_acf(path):
-    pass
+    acf = abs(fft.ifft2(ps))
+    if save:
+        if save == 'fits':
+            from astropy.io import fits
+            fits.tofile(path_to_dat+'_ps.fits', ps)
+            fits.tofile(path_to_dat+'_acf.fits', acf)
+        else:
+            print('Unknown format: {}'.format(save))
+    else:
+        return fft.fftshift(ps), fft.fftshift(acf)    
--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
 
 setup(name='mavr_module',
-      version='0.0.1',
+      version='0.0.2',
       description='SubModules for MAVR SAO RAN',
       author='Anatoly Beskakotov',
       author_email='beskakotov.as@gmail.com',
       url='https://github.com/Black13Wolf/mavr_module',
       packages=['mavr',],
+      install_requires=['matplotlib', 'numpy'],
      )
