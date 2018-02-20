efbde9f6aba6c40881546c8eddef7892c92f0df9
Beskakotov_work <beskakotov.as@gmail.com>
2018-02-20 13:15:04 +0300

    Скопировано описание rebuilder из старых модулей, не протестировано

--- mavr/rebuild.py
+++ mavr/rebuild.py
@@ -28,8 +28,46 @@ def spool(main_path, output_dir, output_name=False, name='spool'):
                 with open(path.join(output_dir, output_name+'.dat'), 'ab') as f:
                     f.write(datfile)
         
-def tif_serie(self):
-    pass
+def serie_tif(self, path_to_dir, output_dir):
+    from PIL import Image
+    from numpy import array, uint16
+    from os import walk, path
+    output_name = path.basename(path_to_dir)
+    files = list(walk(path_to_dir))[0][2]
+    for f in files:
+        if f.endswith('.jpg') or f.endswith('.jpeg') or f.endswith('.png') or f.endswith('.tif') or f.endswith('.gif'):
+            img_path = path.join(path_to_dir, f)
+            img = Image.open(img_path).convert('I')
+            img = array(img).astype('uint16')
+            with open(path.join(output_dir, output), 'ab') as output:
+                output.write(img)
 
-def tif_big(self):
-    pass
+def big_tif(self):
+    from PIL import Image
+    from numpy import array, uint16
+    from os import stat
+    output_name = path.basename(path_to_dir)
+    serie = Image.open(path.join(path_to_dir,'spool.tif'))
+    output_name = path.basename(path_to_dir)
+    tiffs = list(walk(path_to_dir))[0][2]
+    if stat(path.join(path_to_dir, 'spool.tif')).st_size == 8:
+        with open(self.logfile, 'a+') as log:
+            log.write('{}: Ошибка в директории. Пересоберите вручную.\n'.format(self.main_path))
+    elif len(tiffs) == 1:
+        serie = Image.open(path.join(path_to_dir, 'spool.tif'))
+        frames = serie.n_frames
+        for i in range(frames):
+            serie.seek(i)
+            with open(path.join(self.output_dir, output_name+'.dat'), 'ab') as output:
+                output.write(array(serie).astype('uint16'))
+    else:
+        for f in tiffs:
+            if stat(path.join(path_to_dir, f)).st_size == 8:
+                continue
+            else:
+                serie = Image.open(path.join(path_to_dir, f))
+                frames = serie.n_frames
+                for i in range(frames):
+                    serie.seek(i)
+                    with open(path.join(self.output_dir, output_name+'.dat'), 'ab') as output:
+                        output.write(array(serie).astype('uint16'))
